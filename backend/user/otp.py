9eb0fb54cb7e4970a541a0a414967739338058aa
sooshiance <s.katafract@gmail.com>
2024-03-16 22:51:35 +0330

    fix otp bugs

--- backend/user/otp.py
+++ backend/user/otp.py
@@ -1,13 +1,9 @@
 import pyotp 
 
-from .models import UserOTP
-
 
 def otpToken(user, phone):
     totp = pyotp.TOTP(pyotp.random_base32(), interval=60)
 
     otp = totp.now()
 
-    print(f"The OTP is : {otp}") 
-
-    UserOTP.objects.create(user=user, otp=otp)
+    print(f"The OTP is : {otp}")
